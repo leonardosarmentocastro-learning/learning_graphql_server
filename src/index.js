7d3a1c2ada90122d813589eb50cc8667da001242
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2019-03-25 19:36:14 +0100

    simple mutation https://www.howtographql.com/graphql-js/3-a-simple-mutation/
    next:
    exercises for this project at the end of the tutorial
    https://www.howtographql.com/graphql-js/3-a-simple-mutation/

--- src/index.js
+++ src/index.js
@@ -1,39 +1,33 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
-
 let links = [{
   id: 'link-0',
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL'
 }];
+let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => 'This is the API for hacker news clone.',
     feed: (parent) => links,
   },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
 
-  // Link: {
-  //   id: (parent) => {console.log('# parent.id', parent); return `ID: ${parent.id}`},
-  //   description: (parent) => {console.log('# parent.description', parent); return parent.description},
-  //   url: (parent) => {console.log('# parent.url', parent); return parent.url},
-  // }
+      return link;
+    }
+  }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 });
 server.start(() => console.log('server is running on http://localhost:4000'));
