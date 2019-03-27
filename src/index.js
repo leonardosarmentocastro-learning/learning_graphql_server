4ece2c229b8ed241b06469fea9d6f7a725c88692
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2019-03-27 12:29:19 +0100

    Finished CRUD for Link

--- src/index.js
+++ src/index.js
@@ -7,12 +7,34 @@ let links = [{
 }];
 let idCount = links.length;
 
+// operations
+const findById = (list, id) => list.find(item => item.id === id);
+const filterEmptyValues = (fields) =>
+  Object.entries(fields) // desconstruct the object into an array containing each key:value pair
+    // remove entries with empty values
+    .filter(field => {
+      const [ key, value ] = field;
+      return !!value;
+    })
+    // tie each array entry into an object
+    .reduce((prevField, field) => {
+      const [ key, value ] = field;
+      return {
+        ...prevField,
+        [key]: value,
+      };
+    }, {});
+// operations:end
+
 const resolvers = {
   Query: {
-    info: () => 'This is the API for hacker news clone.',
     feed: (parent) => links,
+    link: (parent, args) => findById(links, args.id),
+
+    info: () => 'This is the API for hacker news clone.',
   },
   Mutation: {
+    deleteLink: (parent, args) => links.filter(link => link.id === args.id),
     post: (parent, args) => {
       const link = {
         id: `link-${idCount++}`,
@@ -22,7 +44,22 @@ const resolvers = {
       links.push(link);
 
       return link;
-    }
+    },
+    updateLink: (parent, args) => {
+      const { id, ...fields } = args;
+
+      // Atomically update the "links" array.
+      links = links.map(link => {
+        const isTheLinkImLookingFor = (link.id === id);
+        if (!isTheLinkImLookingFor) return link;
+
+        const updatedLink = { ...link, ...fields };
+        return updatedLink;
+      });
+
+      const updatedLink = links.find(link => link.id === id);
+      return updatedLink;
+    },
   }
 };
 
